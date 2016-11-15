f37920cbb587fd1ce513226520c6e6bc9830c802
Matthias Vandermaesen <matthias@colada.be>
2016-11-15 16:05:56 +0100

    added: basic installer script

--- src/Flemishartcollection/TMSSync/Installer.php
+++ src/Flemishartcollection/TMSSync/Installer.php
@@ -13,11 +13,13 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Flemishartcollection\TMSSync\Database\Connection;
+use Doctrine\DBAL\Schema\Schema;
 
 class Installer extends Command {
+    private $connection;
 
     public function setConnection(Connection $connection) {
-        $connection->getConnection();
+        $this->connection = $connection->getConnection();
     }
 
     /**
@@ -32,6 +34,24 @@ class Installer extends Command {
      * {@inheritdoc}
      */
     protected function execute(InputInterface $input, OutputInterface $output) {
-        var_dump('test');
+        $schema = new Schema();
+
+        /* Now use the Schema object to create a 'users' table */
+        $usersTable = $schema->createTable("users");
+        /* Add some columns to the table */
+        $usersTable->addColumn("id", "integer", array("unsigned" => true));
+        $usersTable->addColumn("first_name", "string", array("length" => 64));
+        $usersTable->addColumn("last_name", "string", array("length" => 64));
+        $usersTable->addColumn("email", "string", array("length" => 256));
+        $usersTable->addColumn("website", "string", array("length" => 256));
+        /* Add a primary key */
+        $usersTable->setPrimaryKey(array("id"));
+
+        $platform = $this->connection->getDatabasePlatform();
+        $queries = $schema->toSql($platform);
+
+        foreach ($queries as $query) {
+            $this->connection->query($query);
+        }
     }
 }
