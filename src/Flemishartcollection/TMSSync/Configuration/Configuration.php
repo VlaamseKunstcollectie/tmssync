2846d9f746d1aa29de5af8e48e180de8e376aee4
Matthias Vandermaesen <matthias@colada.be>
2016-11-16 17:57:15 +0100

    added: comments and error handling for Configuration

--- src/Flemishartcollection/TMSSync/Configuration/Configuration.php
+++ src/Flemishartcollection/TMSSync/Configuration/Configuration.php
@@ -14,35 +14,72 @@ use Symfony\Component\Config\Definition\Processor;
 use Flemishartcollection\TMSSync\Configuration\DatabaseConfiguration;
 use Flemishartcollection\TMSSync\Configuration\SchemaConfiguration;
 
+/**
+ * Configuration manager for Symfony\Config.
+ *
+ * This class wires the Symfony\Config component in the app. It loads config
+ * files stored in app/config, validates them and constructs an array of params
+ * which are used throughout the app.
+ *
+ * @author Matthias Vandermaesen <matthias@colada.be>
+ */
 class Configuration {
+    /**
+     * Instance of Symfony\Component\Config\Definition\Processor
+     */
     private $processor;
 
+    /**
+     * The database configuration validator class
+     */
     private $databaseConfiguration;
 
+    /**
+     * The database schema configuration validator class
+     */
+    private $schemaConfiguration;
+
+    /**
+     * Constructor
+     *
+     * @param Symfony\Component\Config\Definition\Processor $processor The processor
+     */
     public function __construct(Processor $processor) {
         $this->processor = $processor;
         $this->databaseConfiguration = new DatabaseConfiguration();
         $this->schemaConfiguration = new SchemaConfiguration();
     }
 
+    /**
+     * Process the global configuration
+     *
+     * Process the YAML configuration files in app/config. Load them, validate
+     * them and process them through the Symfony\Config component API. Returns
+     * a validated array which is used through the application.
+     *
+     * @return array
+     */
     public function process() {
         $configuration = [];
 
+        // Lookup table for essential config files.
         $files = [
             'config.yml' => 'databaseConfiguration',
             'schema.yml' => 'schemaConfiguration'
         ];
 
         foreach ($files as $file => $processorClass) {
-            try {
-                $basepath = __DIR__ .'/../../../../app/config';
-                $contents = file_get_contents($basepath . '/' . $file);
-                $YMLConfiguration = Yaml::parse($contents);
-            } catch (\InvalidArgumentException $exception) {
-                exit("Are you sure the configuration files exist?");
+            // Fetch each YAML file
+            $basepath = __DIR__ .'/../../../../app/config';
+            $filePath = $basepath . '/' . $file;
+            $contents = @file_get_contents($filePath);
+            if (!$contents) {
+                $msg = sprintf("ERROR: Are you sure the configuration file '%s' exists?", $filePath);
+                exit($msg);
             }
+            $YMLConfiguration = Yaml::parse($contents);
 
-            // Process the configuration files (merge one-or-more *.yml files)
+            // Process a yaml file and add to the global configuration array
             $configuration += $this->processor->processConfiguration(
                 $this->{$processorClass},
                 $YMLConfiguration
@@ -50,6 +87,5 @@ class Configuration {
         }
 
         return $configuration;
- /// TO BE CONTIONUED http://www.andrew-kirkpatrick.com/2014/08/example-use-symfony-config-component-standalone/
     }
 }
