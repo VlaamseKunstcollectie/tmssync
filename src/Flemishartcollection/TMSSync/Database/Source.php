d484a39f9fffa1e6ff6b84d881e7497724f7d5a1
Matthias Vandermaesen <matthias@colada.be>
2016-12-09 11:30:03 +0100

    fixed: CSV header does not match fetched data

--- src/Flemishartcollection/TMSSync/Database/Source.php
+++ src/Flemishartcollection/TMSSync/Database/Source.php
@@ -81,10 +81,6 @@ class Source implements DatabaseInterface {
             $header = array_keys($rows[0]);
 
             if (isset($tables[$destination])) {
-                // $header = array_map(function ($props) {
-                //    return $props['name'];
-                // }, $tables[$destination]['columns']);
-
                 $csv = new CSVWriter();
                 $csv->createCSV($destination);
                 $csv->setHeader($header);
