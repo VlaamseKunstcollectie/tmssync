c5a224f636806b0385cb98b961155a3c30e70aa6
Matthias Vandermaesen <matthias@colada.be>
2016-12-12 14:00:44 +0100

    fixed: missing dep

--- src/Flemishartcollection/TMSSync/Exporter.php
+++ src/Flemishartcollection/TMSSync/Exporter.php
@@ -13,6 +13,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Bridge\Monolog\Handler\ConsoleHandler;
 use Flemishartcollection\TMSSync\Database\Destination;
 use Flemishartcollection\TMSSync\Database\Source;
 use Monolog\Logger;
